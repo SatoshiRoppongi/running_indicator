f45f4445d6eb2ebb719d939f1d2e85053a470876
Satoshi Roppongi <satoshiroppongi@gmail.com>
2020-01-23 00:59:10 +0900

    時間の調整など

--- running_indicator.py
+++ running_indicator.py
@@ -84,9 +84,15 @@ def annotate(clip, txt, text_color='red', fontsize=200, font='Xolonium-Bold', po
 
 
 video = VideoFileClip(file_path)
+print("video.duration")
+print(video.duration)
+end_time = video.duration
 
 # ラップタイム履歴
 laptime_history = []
+
+# ADJUSTMENT_END_TIME = 20  # 終了時間20秒の調整
+# splits_metric[-1]['elapsed_time'] = splits_metric[-1]['elapsed_time'] - ADJUSTMENT_END_TIME
 
 # 行間隔
 LINE_SPACING = 3
@@ -98,14 +104,14 @@ for i, split_info in enumerate(splits_metric):
     laptime_history.append(text_info)
 
 
-print(total_time_list)
+pprint.pprint(splits_metric)
 
 # todo: 以下定数は外だしにしたい
-DISPLAY_TIME = 10  # 10秒間表示する
+DISPLAY_TIME = 5  # 10秒間表示する
 TIME_SPEED = 6  # 何倍速にするか。
 FLASH_FREQ = 2  # 0.25  # 1秒間に何回点滅するか
 interval = 1 / FLASH_FREQ  # 点滅間隔(1回点滅するのにかかる時間)
-center_time_text = [((split_info['total_time']/TIME_SPEED, split_info['total_time']/TIME_SPEED + DISPLAY_TIME), change_time_format(str(split_info['average_speed'])))
+center_time_text = [((min(int(split_info['total_time']/TIME_SPEED), end_time), min(int(split_info['total_time']/TIME_SPEED) + DISPLAY_TIME, end_time)), change_time_format(str(split_info['average_speed'])))
                     for split_info in splits_metric]
 
 for i, time_text in enumerate(center_time_text):
@@ -124,7 +130,7 @@ for center_time_text_item in center_time_text:
         (center_time_text_item[0][0], center_time_text_item[0][0] + interval / 2), center_time_text_item[1])
     center_time_text_flashing.append(prev_time_text)
     t = 0
-    while t <= FLASH_FREQ * TIME_SPEED:
+    while t < FLASH_FREQ * TIME_SPEED:
         time_text_flashing_part = (
             (prev_time_text[0][0] + interval, prev_time_text[0][1] + interval), prev_time_text[1])
         center_time_text_flashing.append(time_text_flashing_part)
@@ -146,29 +152,39 @@ for time_text in center_time_text[1:]:
     # prev_time_text = time_text
     prev_time_text = copy.deepcopy(time_text)
 
-#complemented_time_text_flashing = []
+# complemented_time_text_flashing = []
 # for time_text_item in complemented_time_text:
 
 # center_time_text.pop()
 print("最終的な値")
 print(complemented_time_text)
 # shit code below
-complemented_time_text = complemented_time_text[:-6]
-print("後半の一部を除外")
-print(complemented_time_text)
-annotated_center_laptime_clips = [annotate(video.subclip(from_t, to_t), txt) for (
+# complemented_time_text = complemented_time_text[:-6]
+# print("後半の一部を除外")
+# print(complemented_time_text)
+annotated_center_laptime_clips = [annotate(video.subclip(min(from_t, end_time), min(to_t, end_time)), txt) for (
     from_t, to_t), txt in complemented_time_text]  # 動画と字幕を繋げる処理
 video = editor.concatenate_videoclips(annotated_center_laptime_clips)
 
+print("laptime_history")
+print(laptime_history)
+i = 0
 for lap_text in laptime_history:
-    lap = [((0, lap_text['display_start']), ''),
-           ((lap_text['display_start'], lap_text['display_end']), lap_text['time_text'])]
-    print('rap')
+    i = i + 1
+    print(i)
+    lap = [((0, lap_text['display_start']), ' '),
+           ((min(lap_text['display_start'], end_time), min(lap_text['display_end'], end_time)), lap_text['time_text'])]
+    print('lap pos')
     print(lap)
+    if i == 9:  # for debug
+        break
+
     annotated_laptime_history_subclip = [annotate(video.subclip(
-        from_t, to_t), txt, pos=lap_text['pos'], relative=False, fontsize=10) for (from_t, to_t), txt in lap]
+        min(from_t, end_time), min(to_t, end_time)), txt, pos=lap_text['pos'], relative=False, fontsize=10)
+        for (from_t, to_t), txt in lap]
     video = editor.concatenate_videoclips(annotated_laptime_history_subclip)
 
 final_clip = video
-final_clip.write_videofile("movie_withSubtitle.mp4")
-#final_clip.write_videofile("testVideo.mp4", fps=1)
+final_clip.write_videofile("movie_withSubtitle.mp4",
+                           threads=4, audio=False)
+# final_clip.write_videofile("testVideo.mp4", fps=1)
